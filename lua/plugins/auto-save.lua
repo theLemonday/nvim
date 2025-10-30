da2e34c610ee47d855a92de9e98599dcbc122aef
theLemonday <nhathao090703@gmail.com>
2025-10-30 10:55:29 +0700

    feat: grugfar

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,4 +1,36 @@
-local autoSaveExcludedFiletypess = { "oil" }
+-- some recommended exclusions. you can use `:lua print(vim.bo.filetype)` to
+-- get the filetype string of the current buffer
+local excluded_filetypes = {
+	-- this one is especially useful if you use neovim as a commit message editor
+	"gitcommit",
+	-- most of these are usually set to non-modifiable, which prevents autosaving
+	-- by default, but it doesn't hurt to be extra safe.
+	"NvimTree",
+	"Outline",
+	"TelescopePrompt",
+	"alpha",
+	"dashboard",
+	"lazygit",
+	"neo-tree",
+	"oil",
+	"prompt",
+	"toggleterm",
+}
+
+local excluded_filenames = {
+	"do-not-autosave-me.lua",
+}
+
+local function save_condition(buf)
+	if
+		vim.tbl_contains(excluded_filetypes, vim.fn.getbufvar(buf, "&filetype"))
+		or vim.tbl_contains(excluded_filenames, vim.fn.expand("%:t"))
+	then
+		return false
+	end
+	return true
+end
+
 local group = vim.api.nvim_create_augroup("autosave", {})
 
 vim.api.nvim_create_autocmd("User", {
@@ -6,9 +38,11 @@ vim.api.nvim_create_autocmd("User", {
 	group = group,
 	callback = function(opts)
 		if opts.data.saved_buffer ~= nil then
-			local filename = vim.fn.expand("%:t")
-
-			vim.notify("Auto saved " .. filename, vim.log.levels.INFO)
+			local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+			vim.notify(
+				"AutoSave: saved " .. filename .. " at " .. vim.fn.strftime("%H:%M:%S"),
+				vim.log.levels.INFO
+			)
 		end
 	end,
 })
@@ -19,16 +53,7 @@ return {
 	cmd = "ASToggle", -- optional for lazy loading on command
 	event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
 	opts = {
-		condition = function(buf)
-			local fn = vim.fn
-			local utils = require("auto-save.utils.data")
-
-			-- don't save for file types
-			if utils.not_in(fn.getbufvar(buf, "&filetype"), autoSaveExcludedFiletypess) then
-				return true
-			end
-			return false
-		end,
+		condition = save_condition,
 		debouce_delay = 2000,
 	},
 }
